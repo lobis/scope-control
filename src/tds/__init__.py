f1bc8823f90b41df8c809a12e82771f0110cac53
Luis Antonio Obis Aparicio <luis.antonio.obis@gmail.com>
2023-09-07 13:22:58 +0200

    add scope class

--- src/tds/__init__.py
+++ src/tds/__init__.py
@@ -1,3 +1,4 @@
 from .version import __version__
+from .scope import Scope
 
-__all__ = ["__version__"]
+__all__ = ["__version__", "Scope"]
---
+++ src/tds/scope.py
@@ -0,0 +1,36 @@
+import pyvisa
+import numpy as np
+import time
+
+
+class Scope:
+    def __init__(self, ip_address):
+        self.rm = pyvisa.ResourceManager("@py")
+        self.scope = self.rm.open_resource(
+            f"TCPIP::{ip_address}::INSTR"
+        )
+        self.scope.read_termination = "\n"
+        self.scope.write_termination = "\n"
+
+        self.command_history = []
+
+        self.write("WFMOutpre:FORMat BYTE")  # Set the waveform format to BYTE
+        self.write("WFMOutpre:BYT_Nr 1")  # Set the byte order to LSB
+
+    def write(self, command):
+        self.command_history.append((time.time(), command))
+        print(f"Writing: {command}")
+        self.scope.write(command)
+
+    def print_command_history(self):
+        for timestamp, command in self.command_history:
+            timestring = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(timestamp))
+            print(f"- {timestring}: {command}")
+
+    @property
+    def resource(self):
+        return self.scope
+
+    @property
+    def identity(self):
+        return self.scope.query("*IDN?")
